67f9242828fe5b321e27b410ca449cf80f5433de
Ines.b <inesberradi@gmail.com>
2024-09-27 09:05:06 +0200

    T

--- BattleShip.Models/obj/Debug/net8.0/BattleShip.Models.AssemblyInfo.cs
+++ BattleShip.Models/obj/Debug/net8.0/BattleShip.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleShip.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7779903a6828f57a0fcfc485229495d4cf6c212e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4222fc137177ea039227c213e38c6f4ecafdb79d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleShip.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleShip.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
